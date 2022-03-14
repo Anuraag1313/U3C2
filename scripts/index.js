9ccba162d09b90f2054b234d39c945cf47f5409f
Anuraag <argk1313@gmail.com>
2022-03-14 16:47:31 +0530

    3nd commit

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,7 @@ let data;
 async function getData () {
     let response = await fetch (url);
     data = await response.json();
-    console.log(data[0]["meals"]);
+    //console.log(data[0]["meals"]);
     data = data[0]["meals"];
     data = JSON.stringify(data);
     localStorage.setItem("cart", data);
@@ -21,5 +21,22 @@ async function getData () {
 
 getData();
 
+var dataObj = localStorage.getItem("cart");
+dataObj = JSON.parse(dataObj);
 
 
+
+//console.log(dataObj[0]["strMealThumb"])
+
+
+document.getElementById("item1").setAttribute("src", dataObj[0]["strMealThumb"]);
+document.getElementById("item2").setAttribute("src", dataObj[1]["strMealThumb"]);
+document.getElementById("item3").setAttribute("src", dataObj[2]["strMealThumb"]);
+document.getElementById("item4").setAttribute("src", dataObj[3]["strMealThumb"]);
+document.getElementById("item5").setAttribute("src", dataObj[4]["strMealThumb"]);
+document.getElementById("item6").setAttribute("src", dataObj[5]["strMealThumb"]);
+document.getElementById("item7").setAttribute("src", dataObj[6]["strMealThumb"]);
+document.getElementById("item8").setAttribute("src", dataObj[7]["strMealThumb"]);
+document.getElementById("item9").setAttribute("src", dataObj[8]["strMealThumb"]);
+document.getElementById("item10").setAttribute("src", dataObj[9]["strMealThumb"]);
+document.getElementById("item11").setAttribute("src", dataObj[10]["strMealThumb"]);
